743676f341ca487263710ec67552a148d0337b43
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-02-20 15:53:08 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const request = require('request');
+
+let login = (url, username, password) => {
+	return new Promise((resolve, reject) => {
+		request.get(
+			url,
+			{
+				auth: {
+					user: username,
+					password: password,
+				},
+			},
+			(err, res, body) => {
+				if (err) {
+					reject(err);
+				} else if (res) {
+					if (res.statusCode == 200) {
+						resolve(res);
+					} else {
+						reject(res);
+					}
+					resolve(res.statusCode);
+				} else {
+					reject(new Error(body));
+				}
+			}
+		);
+	});
+};
+
+module.exports.login = login;
